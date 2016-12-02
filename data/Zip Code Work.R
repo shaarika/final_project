d3ce8c4d6672b073fbdb611adbfe002ae4f90dfe
Ishan Narula <ishan.narula@gmail.com>
2016-12-01 22:41:09 -0800

    Added final police file with lat/long

--- data/Zip Code Work.R
+++
@@ -1,7 +0,0 @@
-install.packages("zipcode")
-PolicePractice <- read.csv("PoliceKilling.csv")
-data("zipcode")
-PolicePractice$zip = clean.zipcodes(PolicePractice$Location.of.death..zip.code.)
-Police.Killings = merge(PolicePractice, zipcode, by.x = 'zip', by.y = 'zip') 
-Police <- select(Police.Killings, Date.of.injury.resulting.in.death..month.day.year., Victim.s.age, Victim.s.gender, Victim.s.race, 
-                 Cause.of.death, Unarmed, city, state, zip, latitude, longitude)
