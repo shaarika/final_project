6a71808cee202e0e0b8bce06ece35c62b130a9db
Shaarika Kaul <Shaarika Kaul>
2016-12-01 20:47:10 -0800

    age range

--- summary/retrieval.r
+++ summary/retrieval.r
@@ -1,3 +1,19 @@
 # Shaarika
 # Retrieving summary information
+library(dplyr)
+
+shooting.data  <- read.csv("data/police_killings.csv", stringsAsFactors = FALSE)
+is.data.frame(data)
+
+
+
+# Ages range
+age.data <- filter(shooting.data, Victim.s.age != "Unknown")
+
+# oldest not working
+oldest <- select(age.data, Victim.s.age) %>%
+  filter(Victim.s.age == max(Victim.s.age))
+
+youngest <- select(age.data, Victim.s.age) %>%
+  filter(Victim.s.age == min(Victim.s.age))
 
