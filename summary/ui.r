3c5d6f1aca10b604312cf4edea93be58c36c29e6
Shaarika Kaul <Shaarika Kaul>
2016-12-06 14:22:27 -0800

    formatted text

--- summary/ui.r
+++ summary/ui.r
@@ -6,12 +6,13 @@ shinyUI(fluidPage(
     mainPanel(
       h1("Summary"),
       p("Police killings have affected many individuals in the United States. With the rise of police brutality accusations, the exploration of implicit biases, 
-        and movements against inequity in recent years, our group decided to explore the data and trends regarding these sensitive events. We want to show the data free of bias, 
-        and our analysis will visualize trends in data."),
+        and movements against inequity in recent years, our group decided to explore the data and trends regarding these sensitive events. We want to show the data as free of bias
+        as possible, and our analysis will visualize trends in data."),
       
       p("The dataset we are working with is from mappingpoliceviolence.org, and contains details of police killings in the United States from 2013-2016. 
-        As per their website, "Mapping Police Violence is a research collaborative collecting comprehensive data on police killings nationwide to quantify 
-        the impact of police violence in communities." Their team includes a data scientist, activists, journalists, and teachers.")
+        As per their website, \"Mapping Police Violence is a research collaborative collecting comprehensive data on police killings nationwide to quantify 
+        the impact of police violence in communities.\"
+        Their team includes a data scientist, activists, journalists, and teachers.")
     )
   )
 ))
