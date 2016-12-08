da232d1365becba22d130669b6407a6feeb71241
studentuser <studentuser@D-10-157-220-17.dhcp4.washington.edu>
2016-12-07 23:23:53 -0800

    scatter plot

--- Scatter/server.R
+++ Scatter/server.R
@@ -1,13 +1,38 @@
 library(shiny)
 library(ggplot2)
 library(dplyr)
+library(plotly)
 
-setwd('~/Desktop/final_project/')
-source('./scripts/buildScatter.R')
-data <- read.csv('./data/city_population.csv', stringsAsFactors = FALSE)
+setwd('~/Desktop/final_project')
+
+data <- read.csv('data/city_population.csv')
 data <- as.data.frame(data)
 
-library(dplyr)
+### Build Scatter ###
+BuildScatter <- function(df,  search = '', 
+                         xvar = 'Black.Population.Percentage', yvar = 'Black.Victim.Percentage') {
+  # Filter down to state(s), then graphs
+  # Get x and y max
+  xmax <- max(df[,xvar]) * 1.5
+  ymax <- max(df[,yvar]) * 1.5
+  x.equation <- paste0('~', xvar)
+  y.equation <- paste0('~', yvar)
+  df <- df %>% 
+    filter(grepl(search, State))
+  
+  plot_ly(data = df, x = eval(parse(text = x.equation)), 
+          y = eval(parse(text = y.equation)), 
+          mode='markers', 
+          marker = list(
+            opacity = .4, 
+            size = 10
+          )) %>% 
+    layout(xaxis = list(range = c(0, xmax), title = "Black Population Percentage"), 
+           yaxis = list(range = c(0, ymax), title = "Black Victim Percentage")
+    ) %>% 
+    return()
+}
+
 data.summary <- data %>% 
   group_by(State) %>%
   summarise(Total.Population = sum(Total.Population),
@@ -16,18 +41,24 @@ data.summary <- data %>%
             Total.Killed.by.Police = sum (All.People.Killed.by.Police..2013.2015.),
             Black.Killed.by.Police = sum(Black.People.Killed.by.Police..2013.2015.))
 
-# Adding columns of Black Population percentage of each state & Black Killed Population of Police Brutality Cases
-data.summary <- data.summary %>%
+
+# Adding columns of Black Population percentage of each state & 
+#Black Killed Population of Police Brutality Cases
+
+df <- data.summary %>%
   filter(State != "United States") %>%
   mutate(Black.Population.Percentage = Black.Population / Total.Population * 100) %>%
   mutate(Black.Victim.Percentage = Black.Killed.by.Police / Total.Killed.by.Police * 100)
 
 # Start shinyServer
-shinyServer(function(input, output) { 
+shinyServer(function(input, output, session) { 
   
   # Render a plotly object that returns your map
   output$scatter <- renderPlotly({
-    return(BuildScatter(data.summary, input$state))
+      return(BuildScatter(df, input$search))
+    #}
   })
 })
+
+
 
--- Scatter/ui.R
+++ Scatter/ui.R
@@ -2,31 +2,27 @@ library(shiny)
 library(plotly)
 library(dplyr)
 
-#user interface for States
-shinyUI(fluidPage(
-  
-  # Application title
-  titlePanel("Police Brutality in States"),
-  
-  # Sidebar with a slider input for number of bins
-  
-  sidebarPanel(
-    h3("States"),
-    # Select Justices name here
-    selectizeInput("state", label = "State Name(s)",
-                   choices = unique(data.summary$State), multiple = T,
-                   options = list(maxItems = 30, placeholder = 'Select a state'),
-                   selected = "Washington"),
-    # Term plot
-    plotOutput("termPlot", height = 200),
-    
-    helpText("Data: Showcase the percentage of black population in the state .
-             âEach state has a different population.â Journal of Conflict Resolution. ")
-  ),
-  
-  # Show a plot of the generated distribution
-  mainPanel(
-    plotlyOutput("scatter")
-  )
+shinyUI(navbarPage('Electoral College',
+                   
+                   # Create a tabPanel to show your scatter plot
+                   tabPanel('Scatter',
+                            # Add a titlePanel to your tab
+                            titlePanel('Black Population vs Black Victim'),
+                            
+                            # Create a sidebar layout for this tab (page)
+                            sidebarLayout(
+                              
+                              # Create a sidebarPanel for your controls
+                              sidebarPanel(
+                                
+                                # Make a textInput widget for searching for a state in your scatter plot
+                                textInput('search', label="Find a State", value = '')
+                              ),
+                              
+                              # Create a main panel, in which you should display your plotly Scatter plot
+                              mainPanel(
+                                plotlyOutput('scatter')
+                              )
+                            )
+                   )
 ))
-
