b9d3c661d1ee6540fa6eca867907a8a95d132d7b
Shaarika Kaul <Shaarika Kaul>
2016-12-08 18:33:10 -0800

    WE ARE DONE. OFFICIALLY :D

--- main/server.R
+++ main/server.R
@@ -126,31 +126,11 @@ shinyServer(function(input, output, session) {
     # Render a plotly object that returns your map
     output$scatter <- renderPlotly({
       return(BuildScatter(df, input$search))
-      
+      #}
     })
   
-#*****************************PIE CHART******************************************************
-    # Reading in Data
-    police_final <-read.csv("data/police_final.csv")
-    police_final <- as.data.frame(police_final)
-    
-    # Create a function that allows the user to input a variable from the data which produces a pie
-    # chart that depecits the desired variable and count
-    Pie <- function(col_name){
-      police_final %>%
-        group_by_(col_name = col_name) %>%
-        summarise(count=n()) %>%
-        as.data.frame(col_name) %>%
-        plot_ly(labels = ~col_name, values = ~count, type = 'pie') %>%
-        layout(
-          xaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE),
-          yaxis = list(showgrid = FALSE, zeroline = FALSE, showticklabels = FALSE))}    
-   
-    output$plot <- renderPlotly(Pie(input$variable)
-    )
-    })
-
-
   
+  ####ENDING BRACKETS####  
+})
 
 
